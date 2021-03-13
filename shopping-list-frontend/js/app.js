2ac06cabdf61a8be1c07c18a6ebe98d39798acf8
usharik <usharik@mail.ru>
2021-03-13 22:55:39 +0100

    Relative API links in frontend

--- shopping-list-frontend/js/app.js
+++ shopping-list-frontend/js/app.js
@@ -2,7 +2,7 @@ let ShoppingListApp = angular.module('ShoppingListApp', []);
 
 ShoppingListApp.controller('ShoppingListController', function ($scope, $http) {
 
-    $http.get("http://localhost:8080/api/v1/item/all")
+    $http.get("api/v1/item/all")
         .then(resp => {
                 $scope.shoppingItemList = resp.data;
             },
@@ -14,7 +14,7 @@ ShoppingListApp.controller('ShoppingListController', function ($scope, $http) {
 
     $scope.create = function (name) {
 
-        $http.post("http://localhost:8080/api/v1/item", {'name': name})
+        $http.post("api/v1/item", {'name': name})
             .then(resp => {
                     $scope.shoppingItemList.push(resp.data);
                 },
@@ -26,7 +26,7 @@ ShoppingListApp.controller('ShoppingListController', function ($scope, $http) {
     }
 
     $scope.delete = function (item) {
-        $http.delete("http://localhost:8080/api/v1/item/" + item.id)
+        $http.delete("api/v1/item/" + item.id)
             .then(resp => {
                     let ix = $scope.shoppingItemList.map(item => item.id).indexOf(item.id);
                     $scope.shoppingItemList.splice(ix, 1);
